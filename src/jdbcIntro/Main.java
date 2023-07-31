25111b5dccca5120b14f602850c9b4f8d4c8b580
Fatih Şahin <94977459+ssahinfatih@users.noreply.github.com>
2023-08-01 01:37:14 +0300

    jdbcIntor 2

--- src/jdbcIntro/Main.java
+++ src/jdbcIntro/Main.java
@@ -1,28 +1,117 @@
 package jdbcIntro;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Main {
-	static String userName = "root";
-	static String password = "8520";
-	static String dbUrl = "jdbc:mysql://localhost:3306/sys";
 
-	public static void main(String[] args) throws SQLException{
+	public static void main(String[] args) throws SQLException {
 		Connection connection = null;
-
+		DbHelper helper = new DbHelper();
+		PreparedStatement statement = null;
+		ResultSet resultSet;
 		try {
-			
-			connection = DriverManager.getConnection(dbUrl, userName, password);
-			System.out.println("Bağlantı oluştu");
+
+			connection = helper.getConnection();	
+			String sql= "delete from city where id=?";
+			statement = connection.prepareStatement(sql);
+			statement.setInt(1, 4081);
+		    statement.executeUpdate();
+			System.out.println("Kayıt Silindi");
 
 		} catch (SQLException exception) {
 
-			System.out.println(exception.getMessage());
+			helper.showErrorMessage(exception);
 		} finally {
+			statement.close();
 			connection.close();
 		}
 		
+
+	}
+	
+	public static void selectDemo() throws SQLException {
+		Connection connection = null;
+		DbHelper helper = new DbHelper();
+		Statement statement = null;
+		ResultSet resultSet;
+		try {
+
+			connection = helper.getConnection();
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("SELECT Code,Name,Continent,Region from country ");
+			ArrayList<Country> countries = new ArrayList<Country>();
+
+			while (resultSet.next()) {
+
+				countries.add(new Country(
+						resultSet.getString("Code"), resultSet.getString("Name"),
+						resultSet.getString("Continent"), resultSet.getString("Region")));
+			}
+		
+			
+		System.out.println(countries.size());
+
+		} catch (SQLException exception) {
+
+			helper.showErrorMessage(exception);
+		} finally {
+			connection.close();
+		}
+	}
+	
+	public static void insertData() throws SQLException {
+		Connection connection = null;
+		DbHelper helper = new DbHelper();
+		PreparedStatement statement = null;
+		ResultSet resultSet;
+		try {
+
+			connection = helper.getConnection();	
+			String sql= "insert into city (Name,CountryCode,District,Population) values (?,?,?,?)";
+			statement = connection.prepareStatement(sql);
+			statement.setString(1,"Düzce  2");
+			statement.setString(2,"TUR");
+			statement.setString(3,"Turkey");
+			statement.setInt(4,55000);
+		    statement.executeUpdate();
+			System.out.println("Kayıt Eklendi");
+
+		} catch (SQLException exception) {
+
+			helper.showErrorMessage(exception);
+		} finally {
+			statement.close();
+			connection.close();
+		}
+		
+	}
+	
+	public static void updateData() throws SQLException {
+		Connection connection = null;
+		DbHelper helper = new DbHelper();
+		PreparedStatement statement = null;
+		ResultSet resultSet;
+		try {
+
+			connection = helper.getConnection();	
+			String sql= "update city set population=80001, district='Türkiye' where id=?";
+			statement = connection.prepareStatement(sql);
+			statement.setInt(1, 4085);
+		    statement.executeUpdate();
+			System.out.println("Kayıt Güncellendi");
+
+		} catch (SQLException exception) {
+
+			helper.showErrorMessage(exception);
+		} finally {
+			statement.close();
+			connection.close();
+		}
 	}
 
 }
